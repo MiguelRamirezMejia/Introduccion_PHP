9041686a6eca16d06b8c88ad1e4ffc8f8409e806
MiguelRamirezMejia <miguelramirezcoach@gmail.com>
2023-02-19 02:07:55 -0500

    ejercicio de comentarios e impresiones en pantalla

---
+++ ejercicio1.php
@@ -0,0 +1,18 @@
+<?php
+
+//Este es un comentario de una sola linea
+
+/*
+
+aca puede venir un comentario
+de varias lineas
+
+*/
+
+
+echo "Hola soy Miguel Angel";
+print_r("Hola soy MIguel Angel");
+print("Hola soy Miguel Angel");
+
+
+?>
